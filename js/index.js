07deb3b8ea5492dedc2d3014a02d1581b3cc68b7
Roman <romantylchak29@gmail.com>
2019-07-04 12:07:42 +0300

    V1

--- js/index.js
+++ js/index.js
@@ -4,8 +4,11 @@ var scrolTop;
 if(screen.width == 1366 && screen.height==768 || screen.width == 1270 && screen.height==720){
 	scrolTop = 50;
 }
-if(screen.width == 1920 && screen.height == 1080){
+else if(screen.width == 1920 && screen.height == 1080){
 	scrolTop = 150;
+}
+else{
+	scrolTop = 35;
 }
 
 window.onscroll = function (ev) {	
